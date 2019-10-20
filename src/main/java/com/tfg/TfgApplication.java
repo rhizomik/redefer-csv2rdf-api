08c71ecd7251eecb679e144b7af0c15eabcf8445
Raul Roca <rrj1@alumnes.udl.cat>
2019-10-20 11:41:28 +0200

    Added configuration for allowing File upload via api

--- src/main/java/com/tfg/TfgApplication.java
+++ src/main/java/com/tfg/TfgApplication.java
@@ -1,9 +1,14 @@
 package com.tfg;
 
+import com.tfg.conf.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class TfgApplication {
 
 	public static void main(String[] args) {
