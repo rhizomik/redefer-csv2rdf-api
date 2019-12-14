86f4c9f32231dd507c363c1202cd39ff300a354c
Raul Roca <rrj1@alumnes.udl.cat>
2019-12-14 20:28:57 +0100

    Configured database and web security to be able to acces h2 database

--- src/main/java/com/tfg/conf/WebSecurityConfig.java
+++ src/main/java/com/tfg/conf/WebSecurityConfig.java
@@ -1,5 +1,5 @@
 package com.tfg.conf;
-/*
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -18,11 +18,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Value("${allowed-origins}")
-    String[] allowedOrigins;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable(); // To be able to see h2 console
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
@@ -30,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/upload").authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .httpBasic().realmName("MyTournamentX")
+                .httpBasic().realmName("Tool to improve open data quality")
                 .and()
                 .cors()
                 .and()
@@ -40,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+      //  corsConfiguration.setAllowedOrigins(Arrays.asList(allowedOrigins));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
         corsConfiguration.setAllowCredentials(true);
@@ -50,4 +49,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
-*/
