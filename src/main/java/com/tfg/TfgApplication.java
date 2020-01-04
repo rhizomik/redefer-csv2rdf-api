cd7e378f778c9501e67acbe92d560c811f6c5b35
Raul Roca <rrj1@alumnes.udl.cat>
2020-01-04 13:08:53 +0100

    Deleted unnecessary imports

--- src/main/java/com/tfg/TfgApplication.java
+++ src/main/java/com/tfg/TfgApplication.java
@@ -1,30 +1,19 @@
 package com.tfg;
 
 import com.tfg.conf.StorageProperties;
-import com.tfg.repositories.UserRepository;
-import com.tfg.services.RDFService;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.File;
-import java.util.List;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class TfgApplication {
 
-	@Autowired
-	UserRepository userRepository;
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TfgApplication.class, args);
+
 	/*	RDFService service = new RDFService();
 		List<Model> models = service.createRDF(new File("./src/main/resources/csv/Bienes_declarados_Patrimonio_mundial_de_la_UNESCO_en_España.csv"));
 		for(Model model: models){
